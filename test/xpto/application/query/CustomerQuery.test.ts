8f9bed42531d10454048bb0fdd5a4704af9eaee8
Ana Elisa <anaecouto@outlook.com>
2022-05-03 10:56:18 -0300

    customer query unit testing

--- test/xpto/application/query/CustomerQuery.test.ts
+++ test/xpto/application/query/CustomerQuery.test.ts
@@ -4,20 +4,44 @@ import { CustomerDomain } from 'src/modules/xpto/domain/entity/CustomerDomain';
 import { CustomerDTO } from 'src/modules/xpto/infra/controller/dtos/CustomerDTO';
 import { CustomerRepository } from 'src/modules/xpto/infra/repository/CustomerRepository';
 
-const customerDomainMock: CustomerDomain = {
-  _id: '62712b652ff48bdb383a362b',
-  props: {
-    name: 'Ana Elisa',
-    cpf: '111111111111',
-    phone: '61999999999',
-    birthDate: '2004-05-23',
-    email: 'anaer929292couto@gmail.com',
-  },
-} as CustomerDomain;
-
 describe('customer query', () => {
   let customerQuery: CustomerQuery;
   let customerRepository;
+
+  const customerCreatedMock: CustomerDomain = {
+    _id: '62712b652ff48bdb383a362b',
+    props: {
+      name: 'Ana Elisa',
+      cpf: '111111111111',
+      phone: '61999999999',
+      birthDate: '2004-05-23',
+      email: 'anaer929292couto@gmail.com',
+    },
+  } as CustomerDomain;
+
+  const customerFoundMock = {
+    customer: {
+      name: 'Roberto Pedro Henrique',
+      cpf: '38854441945',
+      phone: '62996442458',
+      birthDate: '1990-03-20',
+      email: 'roberto_almada@luxafit.com.br',
+    },
+  };
+
+  const updatedCustomerMock = {
+    customer: {
+      name: 'Ana Elisa',
+      cpf: '11111111111',
+      phone: '61999987458',
+      birthDate: '1996-08-04',
+      email: 'anaaaaaaaaaaaaa@gmail.com',
+    },
+  };
+
+  const customerDeleted = {
+    message: 'Cliente deletado com sucesso!',
+  };
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -26,7 +50,11 @@ describe('customer query', () => {
         {
           provide: CustomerRepository,
           useValue: {
-            save: jest.fn().mockResolvedValue(customerDomainMock),
+            save: jest.fn().mockResolvedValue(customerCreatedMock),
+            findByCpf: jest.fn().mockResolvedValue(customerFoundMock),
+            findById: jest.fn().mockResolvedValue(customerFoundMock),
+            update: jest.fn().mockResolvedValue(updatedCustomerMock),
+            delete: jest.fn().mockResolvedValue(customerDeleted),
           },
         },
       ],
@@ -34,9 +62,7 @@ describe('customer query', () => {
 
     customerQuery = await module.resolve<CustomerQuery>(CustomerQuery);
 
-    customerRepository = await module.get<CustomerRepository>(
-      CustomerRepository,
-    );
+    module.get<CustomerRepository>(CustomerRepository);
   });
 
   it('should create a new customer', async () => {
@@ -50,6 +76,44 @@ describe('customer query', () => {
 
     const result = await customerQuery.createCustomer(dto);
 
-    expect(result).toEqual(customerDomainMock);
+    expect(result).toEqual(customerCreatedMock);
+  });
+
+  it('should find customer by cpf', async () => {
+    const result = await customerQuery.findCustomer('38854441945');
+
+    expect(result).toEqual(customerFoundMock);
+  });
+
+  it('should find customer by id', async () => {
+    const result = await customerQuery.findById(
+      'c2e2724c-8825-421b-aed6-d570d87bcb93',
+    );
+
+    expect(result).toEqual(customerFoundMock);
+  });
+
+  it('should update customer', async () => {
+    const updateCustomer: CustomerDTO = {
+      name: 'Ana Elisa',
+      cpf: '11111111111',
+      phone: '61999987458',
+      birthDate: '1996-08-04',
+      email: 'anaaaaaaaaaaaaa@gmail.com',
+    };
+    const result = await customerQuery.updateCustomer(
+      '62712b652ff48bdb383a362b',
+      updateCustomer,
+    );
+
+    expect(result).toEqual(updatedCustomerMock);
+  });
+
+  it('should delete customer', async () => {
+    const result = await customerQuery.deleteCustomer(
+      '62712b652ff48bdb383a362b',
+    );
+
+    expect(result).toEqual(customerDeleted);
   });
 });
