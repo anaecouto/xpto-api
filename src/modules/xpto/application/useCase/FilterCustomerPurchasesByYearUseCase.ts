db2f63782a6f97ec3d47e29e5d7cb2788ebe69bb
Ana Elisa <anaecouto@outlook.com>
2022-05-04 17:04:33 -0300

    error handling for use case

--- src/modules/xpto/application/useCase/FilterCustomerPurchasesByYearUseCase.ts
+++ src/modules/xpto/application/useCase/FilterCustomerPurchasesByYearUseCase.ts
@@ -1,17 +1,22 @@
 import { Inject } from '@nestjs/common';
 import AppError from 'src/shared/core/errors/AppError';
 import { ProductsCustomerDomain } from '../../domain/entity/ProductsCustomerDomain';
+import { ICustomerRepo } from '../../domain/repository/ICustomerRepo';
 import { IProductsCustomerRepo } from '../../domain/repository/IProductsCustomerRepo';
+import { CustomerRepository } from '../../infra/repository/CustomerRepository';
 import { ProductsCustomerRepository } from '../../infra/repository/ProductsCustomerRepository';
 
 export class FilterCustomerPurchasesByYearUseCase {
   constructor(
     @Inject(ProductsCustomerRepository)
     private productsCustomerRepository: IProductsCustomerRepo,
+    @Inject(CustomerRepository)
+    private customerRepository: ICustomerRepo,
   ) {}
 
   async execute(id: string, year: string): Promise<ProductsCustomerDomain[]> {
     try {
+      await this.customerRepository.findById(id);
       const purchasesByYear =
         await this.productsCustomerRepository.listAllPurchasesByYear(year);
 
